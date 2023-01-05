5d51220a4898195802b43d6797e7c5723672e460
Mark-andrew7 <kamaumark19@gmail.com>
2023-01-05 06:48:45 -0800

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 for n in range(1, 101):
     if n % 3 == 0 and n % 5 == 0:
-        print("FizzBuzz ", end="")
+        print("{} ".format("FizzBuzz"), end="")
     elif n % 3 == 0:
-        print("Fizz ", end="")
+        print("{} ".format("Fizz"), end="")
     elif n % 5 == 0:
-        print("Buzz ", end="")
+        print("{} ".format("Buzz"), end="")
     else:
-        print("{}".format(n), end="")
+        print("{} ".format(n), end="")
