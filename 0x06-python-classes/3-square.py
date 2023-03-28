791db6bc95e920429af50c34fbba670e91e9d5ff
Mark-andrew7 <kamaumark19@gmail.com>
2023-03-28 10:05:49 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -18,9 +18,8 @@ class Square:
     self.__size = size
     try:
         type(size) == int
-    except TypeError:
-        print("size must be an integer")
-        raise
+    except:
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
 
