da64a837569bcb725bde431e20107d99ddecc163
Mark-andrew7 <kamaumark19@gmail.com>
2023-03-28 06:55:49 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+no module accepted
+"""
+
+class square:
+    """
+    define a class square with private,
+    instance attribute
+    """
+    def __init__(self, size):
+    """
+    Initializes size instance
+    """
+
+    self.__size = size
