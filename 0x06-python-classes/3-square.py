c199541ad9f8d17e0683ef428d96720e952a8c6e
Mark-andrew7 <kamaumark19@gmail.com>
2023-03-28 10:02:20 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+no module imported
+"""
+
+
+class Square:
+    """
+    define a class
+    """
+    def __init__(self, size)
+    """
+    initializes an optional instance size
+
+    size: must be an integer
+    otherwise raise a TypeError
+    """
+    self.__size = size
+    try:
+        type(size) == int
+    except TypeError:
+        print("size must be an integer")
+        raise
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        public instance method
+        returns current square area
+        """
+        return self.__size ** 2
