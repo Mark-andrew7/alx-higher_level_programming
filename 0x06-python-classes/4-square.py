5eea97e97ab16976303c79c31fa2a99ce0e2cc18
Mark-andrew7 <kamaumark19@gmail.com>
2023-03-28 13:03:55 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -28,9 +28,9 @@ class Square:
         """
         method that sets a private attribute
         """
-        if type(size) != int:
+        if type(value) != int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
