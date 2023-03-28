2d39505fa0c33d61a5113aca0684c398b923bd64
Mark-andrew7 <kamaumark19@gmail.com>
2023-03-28 11:55:33 -0700

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+no module imported
+"""
+
+
+class Square:
+    """
+    defines a class
+    """
+    def __init__(self, size=0):
+        """
+        initializes an optional instance size
+
+        size: private instance attribute
+        """
+    @property
+    def size(self):
+        """
+        method that retrieves attribute
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        method that sets attribute
+        """
+        self.__size = value
+    try:
+        type(size) == int
+    except:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        public instance method
+        returns current square area
+        """
+        return self.__size ** 2
