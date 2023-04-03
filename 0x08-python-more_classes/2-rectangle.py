3be45eab30e7bf8d3c601a476087f6dc440568a4
Mark-andrew7 <kamaumark19@gmail.com>
2023-04-03 07:02:29 -0700

    1-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,11 +6,11 @@ no modules imported
 
 class Rectangle:
     """
-    class that defines an __init__ method
+    defines a class rectangle
     """
     def __init__(self, width=0, height=0):
         """
-        initializes parameter with optional instance
+        initializes parameters with optional instance
 
         width: private instance attribute
         height: private instance attribute
@@ -21,35 +21,23 @@ class Rectangle:
     @property
     def width(self):
         """
-        retrieves a private instance attribute
+        retrieves private instance attribute
         """
         return self.__width
 
     @property
     def height(self):
         """
-        retrieves a private instance attribute
+        retrieves private instance attribute
         """
         return self.__height
 
     @width.setter
     def width(self, value):
         """
-        sets a private instance attribute
-        """
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must >= 0")
-        self.__width = value
-
-    @height.setter
-    def height(self, value):
-        """
-        sets a private instance attribute
+        sets private instance attribute
         """
         if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+            raise ValueError("width must >= 0")
