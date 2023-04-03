b822eb705101ffd7e9d7644cc960bcde6f8b0f79
Mark-andrew7 <kamaumark19@gmail.com>
2023-04-03 10:35:51 -0700

    4-rectangle.py

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""
+no modules imported
+"""
+
+
+class Rectangle:
+    """
+    define a class rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        initialize parameters with optional instance
+        width: private instance attribute
+        height: private instance attribute
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        retrieves private instance attribute
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        retrieves private instance attribute
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """
+        sets private instance attribute
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @height.setter
+    def height(self, value):
+        """
+        sets private instance attribute
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        """
+        returns rectangle area
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        returns rectangle perimeter
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width * self.__height)
+
+    def __str__(self):
+        """
+        print rectangle with character #
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for x in range(self.__width):
+            for j in range(self.__height):
+                print("#", end = "")
+        def __repr__(self):
+            """
+            return string representation of rectangle
+            """
+            return "Rectangle({}, {}").format(self.__width, self.__height)
