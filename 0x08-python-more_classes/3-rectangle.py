8b29c325b67d031d48a3b64ec73bc6bf67820bae
Mark-andrew7 <kamaumark19@gmail.com>
2023-04-03 09:40:29 -0700

    3-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""
+no modules imported
+"""
+
+
+class Rectangle:
+    """
+    define a class rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        initialize parameters with optional instance
+
+        width: private instance attribute
+        height: private instance attribute
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        retrieves private instance attribute
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        retrieves private instance attribute
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """
+        sets private instance attribute
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @height.setter
+    def height(self, value):
+        """
+        sets private instance attribute
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        """
+        returns rectangle area
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        returns rectangle perimeter
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.width + self.__height)
+
+    def __str__(self):
+        """
+        string representation of rectangle with character #
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for x in range(self.__width):
+            for j in range(self.__height):
+                print("#", end="")
