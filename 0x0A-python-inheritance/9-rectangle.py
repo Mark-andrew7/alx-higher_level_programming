ba14aeebe81fc420ac0ae1410eaea4a79bdfe3c1
Mark-andrew7 <kamaumark19@gmail.com>
2023-04-11 10:07:10 -0700

    9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -49,4 +49,4 @@ class Rectangle(BaseGeometry):
         """
         return rectangle description
         """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height))
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
