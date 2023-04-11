020fce33df05ed6d5857564a87e41326f6941375
Mark-andrew7 <kamaumark19@gmail.com>
2023-04-11 06:48:49 -0700

    1-my_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,18 +4,12 @@ no modules imported
 """
 
 
-class List:
+class MyList(list):
     """
-    define a class
+    define a class that inherits
     """
     def print_sorted(self):
         """
         prints a sorted list
         """
         print(sorted(self))
-
-class MyList(List):
-    """
-    inheritance
-    """
-    pass
