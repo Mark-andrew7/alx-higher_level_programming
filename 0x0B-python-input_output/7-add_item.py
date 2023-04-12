425f24f2eaff6a5e8b5050d2404b1507b391f995
Mark-andrew7 <kamaumark19@gmail.com>
2023-04-12 09:57:28 -0700

    8-class_to_json.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+import json module
+"""
+import json
+
+
+
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+no modules imported
+"""
+
+
+def class_to_obj(obj):
+    """
+    returns dictionary description with
+    simple data structure list
+    """
+    return obj.__dict__
