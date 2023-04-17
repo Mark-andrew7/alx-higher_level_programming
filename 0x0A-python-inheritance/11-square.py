1916b640ebfe77ed72c7b35c9f75e434daef5bf8
Mark-andrew7 <kamaumark19@gmail.com>
2023-04-17 01:03:40 -0700

    square class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+import class
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    defines a class square
+    """
+    def __init__(self, size):
+        """
+        initialization
+        """
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """
+        return square description
+        """
+        return "[square] {}/{}".format(self.__size, self.__size)
