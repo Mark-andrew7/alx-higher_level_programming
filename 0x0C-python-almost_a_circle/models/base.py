6b5cdb9262c5c108fbbfa031cc015ddcb2365ced
Mark-andrew7 <kamaumark19@gmail.com>
2023-04-17 19:45:13 -0700

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+inheritance
+"""
+
+
+class Base:
+    """
+    define a class base
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        initialization
+        """
+        if id is not None:
+            self.id = id
+        else:
+            base.__nb_objects += 1
+            self.id = base.__nb_objects
