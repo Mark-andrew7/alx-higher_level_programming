610b99a46acef502137cf8cf9238901f3daaad8c
Mark-andrew7 <kamaumark19@gmail.com>
2023-06-05 05:45:56 -0700

    instances

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -18,8 +18,8 @@ class Rectangle:
         width: private instance attribute
         height: private instance attribute
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @classmethod
