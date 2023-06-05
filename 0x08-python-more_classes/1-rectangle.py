b4cdb36da3a6c469d61e2b0e556bbb6cccb22f9c
Mark-andrew7 <kamaumark19@gmail.com>
2023-06-05 02:28:29 -0700

    corrections

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
         width: private instance attribute
         height: private instance attribute
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
