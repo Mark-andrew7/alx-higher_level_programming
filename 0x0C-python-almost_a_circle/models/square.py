12ad4812b5347429fb9e6e362e1c6232fdae3fce
Mark-andrew7 <kamaumark19@gmail.com>
2023-06-19 22:49:15 -0700

    args and kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,6 +39,20 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+
     def to_dictionary(self):
         """
         return dictionary rep
