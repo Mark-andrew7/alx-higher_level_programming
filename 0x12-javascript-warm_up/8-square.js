40711bed87160784cede42c6f0161b45ce3a700f
Mark-andrew7 <kamaumark19@gmail.com>
2024-03-11 07:50:52 -0700

    Fixed logic

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ if (isNaN(arg)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < arg; i++) {
-    let line = ' ';
+    let line = '';
     for (let j = 0; j < arg; j++) {
       line += 'X';
     }
