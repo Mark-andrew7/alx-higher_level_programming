3e800a3a7f23a58729a177a61cd350affc357ac0
Mark-andrew7 <kamaumark19@gmail.com>
2024-03-12 07:26:13 -0700

    Added logic

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,9 +9,8 @@ class Rectangle {
   }
 
   print () {
-    let line = '';
     for (let i = 0; i < this.height; i++) {
-      line += 'X';
+      let line = '';
       for (let j = 0; j < this.width; j++) {
         line += 'X';
       }
