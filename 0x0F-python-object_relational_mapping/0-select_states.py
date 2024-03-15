e0af188de5bd409cc475e9e6f9aebe0a828d32d5
Mark-andrew7 <kamaumark19@gmail.com>
2024-03-15 01:39:59 -0700

    Fixed pycodestyle issues

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
 """
-MySQLdb module used
+lists all states
 """
-
-
 import MySQLdb
 from sys import argv
 
+
 if __name__ == "__main__":
     db = MySQLdb.connect(
-       username=argv[1],
-       password=argv[2],
-       database=argv[3]),
-       host='localhost',
-       port=3306
+        username=argv[1],
+        password=argv[2],
+        database=argv[3],
+        host='localhost',
+        port=3306
+    )
 
     cur = db.cursor()
-    cur.execute('SELECT * FROM states ORDER BY states.id ASC')
-
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
+    cur.execute('SELECT * FROM states ORDER BY id ASC')
 
     cur.close()
     db.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+List all states starting with N
+"""
 
 
 import MySQLdb
