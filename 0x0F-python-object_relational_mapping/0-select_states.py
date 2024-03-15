680727ae2f57fc72e5cb6cd73ff7e4f092c09df0
Mark-andrew7 <kamaumark19@gmail.com>
2024-03-15 02:38:35 -0700

    Fixed username

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        username=argv[1],
+        user=argv[1],
         password=argv[2],
         database=argv[3],
         host='localhost',
