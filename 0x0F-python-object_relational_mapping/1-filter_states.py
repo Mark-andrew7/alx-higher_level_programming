746fdf45d8018162a5ebf4245ca49eaf861dcac9
Mark-andrew7 <kamaumark19@gmail.com>
2024-03-15 01:04:09 -0700

    Getting and filtering states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        username=argv[1],
+        password=argv[2],
+        database=argv[3],
+        host='localhost',
+        port=3306
+    )
+
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE name LIKE "N%"')
+
+    cur.close()
+    db.close()
