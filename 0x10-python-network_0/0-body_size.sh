4d7907998ce2669ed113cf1ffe4f2dc193f15025
Mark-andrew7 <kamaumark19@gmail.com>
2024-03-27 21:21:59 +0300

    cURL POST parameters

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes a URL, sends a request to that URL
-curl -sI "$1" | wc -c
+curl -s "$1" | wc -c
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes URL as argument sends GET request to the URL
-curl -sI "$1" -H "X-School-User-Id: 98"
+curl -sH "X-School-User-Id: 98" "${1}"
