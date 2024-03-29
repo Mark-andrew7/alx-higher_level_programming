be0f9f3ce34674e40949f931adf6b820f9695a87
Mark-andrew7 <kamaumark19@gmail.com>
2024-03-29 04:54:01 +0300

    Changed request type to post

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {'email': sys.argv[2]}
-    res = requests.get(url, data=value)
+    res = requests.post(url, data=value)
     print(res.text)
